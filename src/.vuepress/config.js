12c8475e96873b9d4bb692f9785289ae8590b3d5
juntaro0202 <zyunn14641@gmail.com>
2020-08-10 16:15:09 +0900

    Initial commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "example title",
+};
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+      {
+        text: "Youtube",
+        link:
+          "https://www.youtube.com/channel/UCa6oNOr1S251sHs8rile4Rw?view_as=subscriber",
+      },
+      { text: "twitter", link: "https://twitter.com/junchann0202" },
+      {
+        text: "More",
+        items: [
+          { text: "blog", link: "http://hitsusya.com/" },
+          { text: "読書会", link: "https://www.read4action.com/" },
+          { text: "著書", link: "https://amzn.to/33Fv7cJ" },
+        ],
+      },
+    ],
+  },
+};
---
+++ src/about/index.md
@@ -0,0 +1,21 @@
+# my profile
+
+---
+
+description: page-b description
+meta:
+
+- name: og:url
+  content: https://example.com/page-b/
+- name: og:type
+  content: website
+- name: og:title
+  content: page-b title
+- name: og:description
+  content: page-b description
+- name: og:image
+  content: /og.png
+
+---
+
+# Page B
